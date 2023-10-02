c2278bd22b6fb78ab36b3c9ad9fd4abc50ec2714
Jahid Hasan <jh3495689@gmail.com>
2023-10-02 13:06:59 +0600

    added all code

--- src/Pages/Home/EditData/EditData.jsx
+++ src/Pages/Home/EditData/EditData.jsx
@@ -16,7 +16,7 @@ const EditData = () => {
 
     useEffect(() => {
         async function fetchData() {
-            const response = await fetch(`https://fahim-crud-server-2rul2egi3-nurmorshed7987-gmailcom.vercel.app/addData?search=${search}&page=${currentPage}&limit=${itemPerPage}`);
+            const response = await fetch(`http://localhost:5000/addData?search=${search}&page=${currentPage}&limit=${itemPerPage}`);
             const data = await response.json();
             setData(data);
         }
