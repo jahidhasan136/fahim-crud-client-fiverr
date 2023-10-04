ccd647721289bff4602e6d9b9ef948e328bef3ed
Jahid Hasan <jh3495689@gmail.com>
2023-10-04 16:30:42 +0600

    added loader

--- src/Pages/Home/EditData/EditData.jsx
+++ src/Pages/Home/EditData/EditData.jsx
@@ -12,6 +12,7 @@ const EditData = () => {
     const [itemPerPage, setItemPerPage] = useState(10)
     const searchRef = useRef(null)
     const [search, setSearch] = useState('')
+    const [isLoading, setLoading] = useState(true)
 
 
     useEffect(() => {
@@ -19,14 +20,13 @@ const EditData = () => {
             const response = await fetch(`http://localhost:5000/addData?search=${search}&page=${currentPage}&limit=${itemPerPage}`);
             const data = await response.json();
             setData(data);
-            console.log(data)
+            setLoading(false)
         }
         fetchData();
     }, [currentPage, itemPerPage, control, search])
 
 
     const totalPages = Math.ceil(totalData / itemPerPage)
-    // console.log(totalPages)
 
     const pageNumbers = [...Array(totalPages).keys()];
 
@@ -37,47 +37,53 @@ const EditData = () => {
 
     return (
         <>
-            <div className="container">
-                <div className="my-10 flex justify-end">
-                    {/* <FilterData></FilterData> */}
-                    <input placeholder="search your data ..." ref={searchRef} onChange={handleSearch} className="border rounded-md px-3 py-2" type="text" />
-                </div>
-                <div>
-                    <div className="overflow-x-auto">
-                        <table className="table table-md">
-                            <thead>
-                                <tr>
-                                    <th>No.</th>
-                                    <th>Name</th>
-                                    <th>Number</th>
-                                    <th>Address</th>
-                                    <th>Product Serial No</th>
-                                    <th>Product Model No</th>
-                                    <th>Product Problem</th>
-                                    <th>Issued Date</th>
-                                    <th>Tentative Date</th>
-                                    <th>Day Left</th>
-                                    <th>Live Status</th>
-                                    <th>Product Status</th>
-                                    <th>Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    data.map((jvcoData, index) => <DataContent key={jvcoData._id} jvcoData={jvcoData} index={index} control={control} setControl={setControl}></DataContent>)
-                                }
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-            <div className="container flex justify-end my-2">
-                {
-                    pageNumbers?.map(number => <button className="px-4 bg-yellow-300 rounded-md mr-2" key={number}
-                        onClick={() => setCurrentPage(number)}
-                    >{number + 1}</button>)
-                }
-            </div>
+            {
+                isLoading ?
+                    <p className="text-center text-7xl text-slate-400 mt-10">Loading....</p>
+                    :
+                    <>
+                        <div className="container">
+                            <div className="my-10 flex justify-end">
+                                <input placeholder="search your data ..." ref={searchRef} onChange={handleSearch} className="border rounded-md px-3 py-2" type="text" />
+                            </div>
+                            <div>
+                                <div className="overflow-x-auto">
+                                    <table className="table table-md">
+                                        <thead>
+                                            <tr>
+                                                <th>No.</th>
+                                                <th>Name</th>
+                                                <th>Number</th>
+                                                <th>Address</th>
+                                                <th>Product Serial No</th>
+                                                <th>Product Model No</th>
+                                                <th>Product Problem</th>
+                                                <th>Issued Date</th>
+                                                <th>Tentative Date</th>
+                                                <th>Day Left</th>
+                                                <th>Live Status</th>
+                                                <th>Product Status</th>
+                                                <th>Actions</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {
+                                                data.map((jvcoData, index) => <DataContent key={jvcoData?._id} jvcoData={jvcoData} index={index} control={control} setControl={setControl}></DataContent>)
+                                            }
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="container flex justify-end my-2">
+                            {
+                                pageNumbers?.map(number => <button className="px-4 bg-yellow-300 rounded-md mr-2" key={number}
+                                    onClick={() => setCurrentPage(number)}
+                                >{number + 1}</button>)
+                            }
+                        </div>
+                    </>
+            }
         </>
     );
 };
